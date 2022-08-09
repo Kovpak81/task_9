0d31aaf63022114adeded0c7e3c78ccfd0d033ba
Kovpak81 <gorodko5@mail.ru>
2022-08-09 07:59:00 +0300

    добавлено немного, но еще не докуонца

--- personGenerator.js
+++ personGenerator.js
@@ -35,6 +35,21 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Вера",
+            "id_2": "Надежда",
+            "id_3": "Любовь",
+            "id_4": "Ольга",
+            "id_5": "Мария",
+            "id_6": "Александра",
+            "id_7": "Светлана",
+            "id_8": "Олеся",
+            "id_9": "Яна",
+            "id_10": "Елена"
+        }
+    }`,
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -60,11 +75,23 @@ const personGenerator = {
 
     },
 
+    randomGender: function() {
+// randomIntNumbe использовать для пола и даты рождения
+        return this.randomIntNumber(this.surnameJson); 
 
-    getPerson: function () {
+    },
+
+    randomBith: function() {
+        const birth = this.randomIntNumber(1900, 2022);  
+        return obj.list[birth];
+    },
+
+       getPerson: function () {
         this.person = {};
         // this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surname = this.randomSurname();
+        
         return this.person;
     }
 };
